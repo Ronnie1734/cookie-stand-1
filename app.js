4badcceceeab266103eb9edd99838e38c0b759cd
Rafael Malave <rafomalave@gmail.com>
2017-10-10 19:10:08 -0700

    Add random number method to objects

--- app.js
+++ app.js
@@ -3,29 +3,54 @@
 var firstAndPike = {
   minCustomers: 23,
   maxCustomers: 65,
-  avgSale: 6.3
+  avgSale: 6.3,
+  getCustomers: function() {
+    var min = Math.ceil(this.minCustomers);
+    var max = Math.floor(this.maxCustomers);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
 };
 
 var alki = {
   minCustomers: 2,
   maxCustomers: 16,
-  avgSale: 4.6
+  avgSale: 4.6,
+  getCustomers: function() {
+    var min = Math.ceil(this.minCustomers);
+    var max = Math.floor(this.maxCustomers);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
 };
 
 var seaTac = {
   minCustomers: 3,
   maxCustomers: 24,
   avgSale: 1.2,
+  getCustomers: function() {
+    var min = Math.ceil(this.minCustomers);
+    var max = Math.floor(this.maxCustomers);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
 };
 
 var seaCenter = {
   minCustomers: 11,
   maxCustomers: 38,
-  avgSale: 3.7
+  avgSale: 3.7,
+  getCustomers: function() {
+    var min = Math.ceil(this.minCustomers);
+    var max = Math.floor(this.maxCustomers);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
 };
 
 var capHill = {
   minCustomers: 20,
   maxCustomers: 38,
-  avgSale: 2.3
+  avgSale: 2.3,
+  getCustomers: function() {
+    var min = Math.ceil(this.minCustomers);
+    var max = Math.floor(this.maxCustomers);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
 };
