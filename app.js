fe37cea63f6e145ae0ed4461efcc8194961d4851
Rafael Malave <rafomalave@gmail.com>
2017-10-10 18:45:21 -0700

    Add html boilerplate and created all location objects

---
+++ app.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var firstAndPike = {
+  minCustomers: 23,
+  maxCustomers: 65,
+  avgSale: 6.3
+};
+
+var alki = {
+  minCustomers: 2,
+  maxCustomers: 16,
+  avgSale: 4.6
+};
+
+var seaTac = {
+  minCustomers: 3,
+  maxCustomers: 24,
+  avgSale: 1.2,
+};
+
+var seaCenter = {
+  minCustomers: 11,
+  maxCustomers: 38,
+  avgSale: 3.7
+};
+
+var capHill = {
+  minCustomers: 20,
+  maxCustomers: 38,
+  avgSale: 2.3
+};
